f7c9db467c196df88f015c3c1753eb3a6b78e924
Ritvik Bhatia <ritvikbhatia99@gmail.com>
2020-08-04 00:39:06 +0530

    Create cifaimagesvm.py

---
+++ cifaimagesvm.py
@@ -0,0 +1,26 @@
+import sklearn
+import sklearn.ensemble
+import sklearn.datasets
+import sklearn.linear_model
+import sklearn.tree
+import pandas as pd
+import numpy as np
+import pylab as pl
+import pickle
+import scipy.misc as smp
+import matplotlib.pyplot as plt
+import sklearn.datasets
+with open("C:\\Users\\Ritvik\\Documents\\datasets\\cifar-100-python\\train","rb") as fo:
+    dict=pickle.load(fo,encoding='latin-1')
+print(dict['data'].shape)
+x=np.array(dict['data'])
+img=(np.reshape(x[7],(3,32,32)))
+img=smp.toimage(img)
+img.show()
+with open("C:\\Users\\Ritvik\\Documents\\datasets\\cifar-100-python/meta",'rb') as fo:
+    meta=pickle.load(fo)
+y=dict['coarse_labels']
+x_train,x_test,y_train,y_test=sklearn.model_selection.train_test_split(x,y,test_size=0.2)
+model=sklearn.svm.SVC(kernel='poly',degree=3)
+model.fit(x_train,y_train)
+print(model.score(x_test,y_test))
